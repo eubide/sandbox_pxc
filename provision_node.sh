dcc191e01cbd1397b42e04a52363f12b5c83c12b
Eduardo Ubide <eubide@gmail.com>
2021-05-11 18:55:26 +0200

    pxc included pigz on sst process

--- provision_node.sh
+++ provision_node.sh
@@ -16,7 +16,8 @@ BOOTSTRAP_IP="$4"
 yum makecache fast
 
 yum -y install yum install https://repo.percona.com/yum/percona-release-latest.noarch.rpm
-yum -y install tar gdb strace vim qpress socat
+yum -y install tar gdb strace perf socat pigz
+yum -y install vim qpress percona-toolkit
 
 # yum -q list available --showduplicates Percona-XtraDB-Cluster-server-57
 # [...]
@@ -29,21 +30,28 @@ yum -y install tar gdb strace vim qpress socat
 # Percona-XtraDB-Cluster-server-57.x86_64                5.7.32-31.47.1.el7
 # Percona-XtraDB-Cluster-server-57.x86_64                5.7.33-31.49.1.el7
 
-yum -y -q install Percona-XtraDB-Cluster-server-57-5.7.31-31.45.3.el7.x86_64
+# yum -y -q install Percona-XtraDB-Cluster-server-57-5.7.31-31.45.3.el7.x86_64
+
+yum -y -q install Percona-XtraDB-Cluster-server-57-5.7.30-31.43.1.el7.x86_64
 
 mysqld --initialize-insecure --user=mysql
 
 tee /etc/my.cnf <<EOF
 [mysql]
+
 port                                = 3306
 socket                              = /var/lib/mysql/mysql.sock
 prompt                              = 'PXC: \u@\h (\d) > '
 
+
 [client]
+
 port                                = 3306
 socket                              = /var/lib/mysql/mysql.sock
 
+
 [mysqld]
+
 socket                              = /var/lib/mysql/mysql.sock
 datadir                             = /var/lib/mysql
 user                                = mysql
@@ -73,10 +81,19 @@ innodb_use_native_aio               = 0
 server_id                           = $NODE_NR
 binlog_format                       = ROW
 
+
 [sst]
+
 streamfmt                           = xbstream
 
+# pigz -- use-memory=32G and increase p to cpu number
+inno_apply_opts                     = "--use-memory=1G"
+compressor                          = "pigz -p2"
+decompressor                        = "pigz -p2 -d"
+
+
 [xtrabackup]
+
 compress
 parallel                            = 2
 compress_threads                    = 2
@@ -84,40 +101,38 @@ rebuild_threads                     = 2
 EOF
 
 if [[ $NODE_NR -eq 1 ]]; then
-	systemctl start mysql@bootstrap
+  systemctl start mysql@bootstrap
 
-	# ProxySQL users
-	mysql -e "CREATE USER 'monitor'@'%' IDENTIFIED BY 'monit0r';"
-	mysql -e "GRANT USAGE ON *.* TO 'monitor'@'%';"
-	mysql -e "CREATE USER 'monitor'@'localhost' IDENTIFIED BY 'monit0r';"
-	mysql -e "GRANT USAGE ON *.* TO 'monitor'@'localhost';"
+  # ProxySQL users
+  mysql -e "CREATE USER 'monitor'@'%' IDENTIFIED BY 'monit0r';"
+  mysql -e "GRANT USAGE ON *.* TO 'monitor'@'%';"
+  mysql -e "CREATE USER 'monitor'@'localhost' IDENTIFIED BY 'monit0r';"
+  mysql -e "GRANT USAGE ON *.* TO 'monitor'@'localhost';"
 
-	mysql -e "CREATE USER 'app'@'%' IDENTIFIED BY 'app';"
-	mysql -e "GRANT ALL ON *.* TO 'app'@'%';"
-	mysql -e "CREATE USER 'app'@'localhost' IDENTIFIED BY 'app';"
-	mysql -e "GRANT ALL ON *.* TO 'app'@'localhost';"
+  mysql -e "CREATE USER 'app'@'%' IDENTIFIED BY 'app';"
+  mysql -e "GRANT ALL ON *.* TO 'app'@'%';"
+  mysql -e "CREATE USER 'app'@'localhost' IDENTIFIED BY 'app';"
+  mysql -e "GRANT ALL ON *.* TO 'app'@'localhost';"
 
-	mysql -e "CREATE USER 'mariabackup'@'localhost' IDENTIFIED BY 'mar1ab4ckup';"
-	mysql -e "GRANT RELOAD, PROCESS, LOCK TABLES, REPLICATION CLIENT ON *.* TO 'mariabackup'@'localhost';"
+  mysql -e "CREATE USER 'mariabackup'@'localhost' IDENTIFIED BY 'mar1ab4ckup';"
+  mysql -e "GRANT RELOAD, PROCESS, LOCK TABLES, REPLICATION CLIENT ON *.* TO 'mariabackup'@'localhost';"
 
-	mysql -e "CREATE DATABASE test;"
+  mysql -e "CREATE DATABASE test;"
 
-	mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'192.%' IDENTIFIED BY 'sekret';"
-	mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'127.0.0.1' IDENTIFIED BY 'sekret';"
-	mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY 'sekret';"
-
-
-
+  mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'192.%' IDENTIFIED BY 'sekret';"
+  mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'127.0.0.1' IDENTIFIED BY 'sekret';"
+  mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY 'sekret';"
 
 else
-	for i in {1..60}; do
-		MYSQLADMIN=$(mysqladmin -uroot -psekret -h"$BOOTSTRAP_IP" ping)
-		if [[ "$MYSQLADMIN" == "mysqld is alive" ]]; then
-			systemctl start mysql
-			echo "ready on $i"
-			exit
-		else
-			sleep 5
-		fi
-	done
+
+  for i in {1..60}; do
+    MYSQLADMIN=$(mysqladmin -uroot -psekret -h"$BOOTSTRAP_IP" ping)
+    if [[ "$MYSQLADMIN" == "mysqld is alive" ]]; then
+      systemctl start mysql
+      echo "ready on $i"
+      exit
+    else
+      sleep 5
+    fi
+  done
 fi
